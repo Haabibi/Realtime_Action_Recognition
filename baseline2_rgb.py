2d6de062242eee13d10f191babebbce36daf195c
Abigail Haakyung Lee <haabibi623@gmail.com>
2019-01-09 13:27:28 +0900

    temp

--- baseline2_rgb.py
+++ baseline2_rgb.py
@@ -33,17 +33,17 @@ def make_hmdb():
     return index_dict
 
 def eval_video(data, length, net, style):
-    print("[these are the parameters of eval_vid]: ", length, data.size(2), data.size(3), data.shape)
     input_var = torch.autograd.Variable(data.view(-1, length, data.size(2), data.size(3)), volatile=True)
+    cuda_tic = time.time()
     input_var = input_var.cuda()
-    print("[eval_vid]: ", type(input_var), input_var.shape) 
+    cuda_toc = time.time()
     rst = net(input_var)
-     
+    time_run_net = time.time()
     rst_data = rst.data
     rst_data_cpu = rst_data.cpu()
     rst_data_np = rst_data_cpu.numpy().copy()
-    print("[rst_data_np]: ", type(rst_data_np), rst_data_np.shape)
-    
+    time_cpu_copy_time = time.time() 
+    print("[eval_video_time]: [cuda]: {}, [run_net]: {}, [cpu_cpy_time]: {}".format(cuda_toc-cuda_tic, time_run_net-cuda_toc, time_cpu_copy_time-time_run_net))
     return rst_data_np.reshape((args.test_crops, args.test_segments, num_class)).mean(axis=0).reshape((args.test_segments, 1, num_class))
 
 def make_infer(style, weights, batched_array, net): 
@@ -62,7 +62,6 @@ def make_infer(style, weights, batched_array, net):
         flow_prefix = 'img'
     else:
         flow_prefix = 'flow_{}'
-    data_tic = time.time() 
     data_loader = torch.utils.data.DataLoader(
            TSNDataSet(batched_array,
                       modality=style,
@@ -75,24 +74,50 @@ def make_infer(style, weights, batched_array, net):
                           GroupNormalize(net.input_mean, net.input_std),
                       ]),test_mode=True),
                batch_size=args.q_size, shuffle=False,
-               num_workers=1,#args.workers * 2,
+               num_workers=args.workers * 2,
                pin_memory=True)
-    answer = make_hmdb() if args.dataset == 'hmdb51' else make_ucf()
-    data_toc = time.time()
+    #answer = make_hmdb() if args.dataset == 'hmdb51' else make_ucf()
     net.float() 
     net.eval() 
+    net_cuda_tic = time.time()
     net = net.cuda() 
-    video_pred_tic = time.time()
-    data = next(iter(data_loader))
-    print("[type of data]: ", type(data))
+    net_cuda_toc = time.time() 
+    print("[net_cuda_time]: ", net_cuda_toc-net_cuda_tic)
+    
+    max_num = args.max_num if args.max_num > 0 else len(data_loader.dataset)
+    enumrate_time = time.time() 
+    data_gen = enumerate(data_loader)
+    video_pred_tic = time.time() 
+    print("[time for enumerating data_loader]: ", video_pred_tic-enumrate_time, "[max_num]: ", max_num)
+    video_pred_tic = time.time() 
+    for i, (data) in data_gen:
+        if i >= max_num:
+            break
+        if style == 'RGB':  
+            rgb_eval_vid_tic = time.time()
+            rst = eval_video(data, 3, net, style)
+            rgb_eval_vid_toc = time.time()
+        else:
+            of_eval_vid_tic = time.time()
+            rst = eval_video(data, 10, net, style)
+            of_eval_vid_toc = time.time()
+    video_pred_toc = time.time() 
+    print("[time actually running for loop]: ", video_pred_toc-video_pred_tic)
+    return rst
+    '''
+    data_load_tic = time.time()
+    iter_data_load = iter(data_loader)
+    data_load_tic_2 = time.time()
+    data = next(iter_data_load)
+    #data = next(iter(data_loader))
+    data_load_toc = time.time()  
+    print("[data_iter time], ",data_load_tic_2 - data_load_tic, "[net_daata], ", data_load_toc-data_load_tic_2 )
     eval_vid_tic = time.time()
     rst = eval_video(data, 3 if style =="RGB" else 5, net, style) 
     eval_vid_toc = time.time()
-    video_pred_toc = time.time() 
-    if style == 'RGB':
-        print("evaluating rgb(NET EVAL) in {:.4f}, {} data_loading in {:.4f}, video_pred in {:.4f}, net_eval and parallelism {:.4f}".format(eval_vid_toc-eval_vid_tic, style, data_toc-data_tic, video_pred_toc-video_pred_tic, video_pred_tic-data_toc))
+    print("[rst, eval_vid]: ", eval_vid_toc-eval_vid_tic)
     return rst 
-
+    '''
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser( 
@@ -146,23 +171,36 @@ if __name__=="__main__":
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
+            frame = frame.reshape(1, 240, 320, 3)
             counter += 1 
             if counter == 1:
-                 inp = np.zeros((240, 320, 3))
+                 first_time_rgb = time.time() 
+                 inp = np.zeros((1, 240, 320, 3))
                  inp += frame
                  print("[c1] inp.shape: ", inp.shape)
             elif counter < args.q_size: 
                  inp = np.concatenate((inp, frame))
                  print("[c{}] inp.shape: ".format(counter), inp.shape)
             elif counter == args.q_size:
+                 ready_for_inf = time.time()
+                 print("[Time when frames are full]: ", ready_for_inf-first_time_rgb)
                  inp = np.concatenate((inp, frame))
-                 inp = inp.reshape((args.q_size, 240, 320, 3))
-                 #print("[c{}] inp.shape: ".format(counter), inp.shape)
-                 rgb_inf_tic = time.time()
-                 rgb_inf_score = make_infer('RGB', args.rgb_weights, inp, rgb_net)
-                 rgb_inf_toc = time.time()
-                 video_pred = np.argmax(np.mean(rgb_inf_score[0], axis=0))
-                 print("THIS IS THE RESULT: ", make_hmdb()[video_pred])
+                 #inp = inp.reshape((args.q_size, 240, 320, 3))
+                 for i in range(args.num_repeat+1):
+                     if i == 0: 
+                         cold_case_tic = time.time()
+                         rgb_inf_score = make_infer('RGB', args.rgb_weights, inp, rgb_net)
+                         cold_case_toc = time.time()
+                         print("[cold case time]: ", cold_case_toc-cold_case_tic)
+                         video_pred = np.argmax(np.mean(rgb_inf_score[0], axis=0))
+                         print("THIS IS THE RESULT: ", make_hmdb()[video_pred])
+                     else:                         
+                         rgb_inf_tic = time.time()
+                         rgb_inf_score = make_infer('RGB', args.rgb_weights, inp, rgb_net)
+                         rgb_inf_toc = time.time()
+                         video_pred = np.argmax(np.mean(rgb_inf_score[0], axis=0))
+                         print("[rgb_inf time]: ", rgb_inf_toc-rgb_inf_tic)
+                         print("THIS IS THE RESULT: ", make_hmdb()[video_pred])
                   
                  counter = 0 
 
