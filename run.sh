670904f2b8e0443dc517f4f7a8089687bd5f2237
Abigail Haakyung Lee <haabibi623@gmail.com>
2019-01-10 22:11:06 +0900

    delete files

--- run.sh
+++
@@ -1,4 +0,0 @@
-ls -d /cmsdata/hdd2/cmslab/haabibi/OPTICAL_FLOW_HMBD/* | while read d 
-do
-    python3 run_inf.py ucf101 RGB $d ucf101_bninception_1_rgb_checkpoint.pth.tar --arch BNInception | tee ~/UCF_outcome.txt
-done
